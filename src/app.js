bcb4d468688f57882be59b43d67ae3ac9ce5b863
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-06-17 17:36:10 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateDomains() {
+  let middle = ["apple", "popcorn", "water"];
+  var prefixes = ["coding", "testcoding", "learncoding"];
+  var suffix = [".com", ".org", ".edu"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (var i = 0; i < prefixes.length; i++)
+    for (var a = 0; a < middle.length; a++)
+      for (var b = 0; b < suffix.length; b++)
+        console.log(prefixes[i] + middle[a] + suffix[b]);
+}
+console.log(generateDomains());
